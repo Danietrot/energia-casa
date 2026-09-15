b7dfe7fdd982b5a04637dc08e608272212269405
Danietrot <trotta.daniele@yahoo.it>
2026-09-15 11:32:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="energia-casa-v4";
+const CACHE="energia-casa-v5";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./tariffe.json"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
